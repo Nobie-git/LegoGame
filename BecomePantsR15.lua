6db56b2f4b97962e039213dffe16152cc4d0a2c3
Nobie <63230056+Nobie-git@users.noreply.github.com>
2021-04-20 08:15:50 +0300

    Create BecomePantsR15.lua

---
+++ BecomePantsR15.lua
@@ -0,0 +1,4 @@
+setsimulationradius(math.huge ^ math.huge, math.huge ^ math.huge)
+game.Players.LocalPlayer.Character.UpperTorso.Waist:Destroy()
+local bp = Instance.new("BodyPosition", game.Players.LocalPlayer.Character.UpperTorso)
+bp.Position = Vector3.new(0,1000,0)
